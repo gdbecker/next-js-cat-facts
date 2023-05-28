ed5f32d6989b459feee6cdd13b67b983b61674ae
Garrett Becker <garrettdbecker@gmail.com>
2023-05-28 16:14:33 -0400

    Updates

--- app/api/images/route.js
+++ app/api/images/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import images from './data.json';
+import images from '../../../data/data.json';
 
 export async function GET(request) {
   var index = Math.floor(0 + Math.random() * (images.length - 1 + 1));
